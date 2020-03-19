7be17ff932cb0803236b863d24f49f67904c26ac
Iosif V <dev@iosifv.com>
2020-03-19 00:21:17 +0000

    [master] - rearanged snaps

--- gen_3-gui-apps.sh
+++ gen_3-gui-apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Cool new terminal
+# Terminal - kitty
 sudo apt install kitty -y -qq
 cd $HOME
 git clone https://github.com/adi1090x/kitty-cat
@@ -8,19 +8,31 @@ cd kitty-cat
 ./install
 cd $HOME
 
-# Terminal Overload
+# Terminal - alacritty
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install alacritty -y -qq
 
-# Glorious Snaps
-sudo snap install docker postman
-sudo snap install aws-cli --classic
-sudo snap install code    --classic
-sudo snap install cointop --stable
-
-# Snaps not needed in case of Virtual machine
-sudo snap install spotify caprine snap-store vlc gitkraken chromium
-sudo snap install slack   --classic
+# Dev
+sudo snap install docker
+sudo snap install postman
+sudo snap install gitkraken
+sudo snap install code     --classic
 sudo snap install phpstorm --classic
 sudo snap install webstorm --classic
-sudo snap install authy --beta
+# sudo snap install aws-cli --classic
+
+# Media
+sudo snap install spotify
+sudo snap install opera
+sudo snap install chromium
+
+# Social
+sudo snap install caprine           # Facebook GUI
+sudo snap install wrapup            # Whatsup GUI
+sudo snap install cordless          # Discord CLI
+sudo snap install slack --classic
+sudo snap install vlc
+
+# Tools
+sudo snap install cointop --stable
+sudo snap install authy   --beta
